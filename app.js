7f4fb1ebc27ebaf892d5a9663805bc242aef02fe
Roney Erdemann <roneyweb@ig.com.br>
2022-11-24 16:49:54 -0300

    Adicionando um console log

--- app.js
+++ app.js
@@ -1,1 +1,3 @@
 console.log("Rodando o sistema de gerenciamento de jogos");
+console.log("Aplicação está se conectado ao banco de dados");
+console.log("O dev fez esse commit sem você ver na aula");
